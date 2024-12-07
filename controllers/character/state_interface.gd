6133f73a768eea3a56c6a4c50bc7622e37f0a33b
Stephen-Zulauf <szulauf1990@gmail.com>
2024-12-06 19:45:49 -0800

    adding more animations to controller

--- controllers/character/state_interface.gd
+++ controllers/character/state_interface.gd
@@ -3,6 +3,8 @@ extends Node3D
 
 @onready var CHART = $"../StateChart"
 @onready var CONTROLLER = $".."
+@onready var SPRING_ARM = $"../X Bot/GeneralSkeleton/BoneAttachment3D/SpringArmPivot/SpringArm3D"
+@export var SPRING_ARM_LENGTH: float = 3.0
 #@export var CONTROLLER: CharacterController
 
 @onready var ANIM_TREE: AnimationTree = $"../AnimationTree"
@@ -13,17 +15,19 @@ extends Node3D
 @export var DECELERATION: float = 0.5
 @export var JUMP_VELOCITY: float = 4.5
 
-var run_speed: float = 7.0
+var run_speed: float = 4.0
 var run_acc: float = 0.9
 
-var walk_speed: float = 2.5
+var walk_speed: float = 1.5
 var walk_acc: float = 1.5
 
-var crouch_speed: float = 2.0
+var crouch_speed: float = 1.0
 var crouch_acc: float = 0.8
 
 var def_acc: float = 1.5
 var def_dec: float = 0.5
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -34,6 +38,9 @@ func _process(delta: float) -> void:
 	CONTROLLER.update_gravity(delta)
 	CONTROLLER.update_input(SPEED,ACCELERATION,DECELERATION)
 	CONTROLLER.update_velocity()
+	
+	SPRING_ARM.set("spring_length", SPRING_ARM_LENGTH)
+	
 
 #check if grounded or in air
 func _on_grounded_state_physics_processing(delta: float) -> void:
@@ -43,6 +50,11 @@ func _on_grounded_state_physics_processing(delta: float) -> void:
 		CHART.send_event("air")
 	elif !CONTROLLER.is_on_floor():
 		CHART.send_event("air")
+	if Input.is_action_just_pressed("camera"):
+		if SPRING_ARM_LENGTH > 0:
+			SPRING_ARM_LENGTH = 0
+		else: 
+			SPRING_ARM_LENGTH = 3
 
 func _on_inair_state_physics_processing(delta: float) -> void:
 	if CONTROLLER.is_on_floor():
@@ -117,6 +129,9 @@ func _on_sprinting_state_physics_processing(delta: float) -> void:
 		CHART.send_event("crouch")
 
 func _on_crouching_state_physics_processing(delta: float) -> void:
+	var rel_vel = CONTROLLER.global_basis.inverse() * ((CONTROLLER.velocity * Vector3(1,0,1))/SPEED)
+	var rel_vel_xz = Vector2(rel_vel.x, -rel_vel.z)
+	ANIM_TREE.set("parameters/crouch/blend_position", rel_vel_xz)
 	if Input.is_action_pressed("sprint"):
 		CHART.send_event("sprint")
 	elif Input.is_action_just_released("crouch"):
